c777a8b291b4a1489d02d934c419baf4ae52c939
Hugo Flores <hfp.ing2@gmail.com>
2020-05-24 18:54:31 -0400

    Network methods added

--- GithubSearcher/View Models/UserSearchViewModel.swift
+++ GithubSearcher/View Models/UserSearchViewModel.swift
@@ -11,8 +11,13 @@ import Foundation
 class UserSearchViewModel {
 
     let noQueryMessage = "Type on the search bar to get users"
-
-    private var usersResponse: UsersResponse? {
+    
+    private let networkManager: NetworkManager!
+    
+    private var cachedImages = NSCache<NSString, NSData>()
+    private var cachedUserDetails = NSCache<NSString, NSString>()
+    
+    private var users: [UserCell] = [] {
         didSet {
             isDataDownloading = false
             guard let handler = reloadTableHandler else { return }
@@ -31,7 +36,7 @@ class UserSearchViewModel {
                 isDataDownloading = false
                 displayMessage = noQueryMessage
                 userQuery = nil
-                usersResponse = nil
+                users = []
                 return
             }
             isDataDownloading = true
@@ -57,6 +62,10 @@ class UserSearchViewModel {
     var setScreenMessageHandler: ((String?) -> Void)?
     var setActivityIndicatorHandler: ((Bool) -> Void)?
     var reloadTableHandler: (() -> Void)?
+    
+    init(networkHandler: NetworkProtocol) {
+        networkManager = NetworkManager(networkHandler: networkHandler)
+    }
 
     func setNewQuery(query: String) {
         userQuery = query
@@ -68,15 +77,62 @@ class UserSearchViewModel {
             DispatchQueue.main.async {
                 switch result {
                 case .success(let data):
-                    self.usersResponse = data
+                    self.users = self.parseResponseToUserCell(usersResponse: data)
                 case .failure(let error):
                     print(error)
                 }
             }
         }
     }
-    
+
+    private func parseResponseToUserCell(usersResponse: UsersResponse) -> [UserCell] {
+        return usersResponse.items.map {
+            let viewModel = UserCell(user: $0)
+            viewModel.avatarDownloadHandler = getUserAvatar
+            viewModel.userDetailsDownloadHandler = getUserDetails
+            return viewModel
+        }
+    }
+
     func getNumberOfUsers() -> Int {
-        return usersResponse?.items.count ?? 0
+        return users.count
+    }
+
+    func getUserViewModelAt(indexPath: IndexPath) -> UserCell {
+        return users[indexPath.row]
+    }
+    
+    private func getUserAvatar(url: String, completion: @escaping (Data?) -> Void) {
+        if let cachedData = cachedImages.object(forKey: url as NSString) {
+            completion(cachedData as Data)
+            return
+        }
+        networkManager.getDataFrom(url: url) { [unowned self] (result) in
+            switch result {
+            case .success(let imgData):
+                completion(imgData)
+                self.cachedImages.setObject(imgData as NSData, forKey: url as NSString)
+            case .failure(let error):
+                print("Error in image download: ", error)
+            }
+        }
+    }
+
+    private func getUserDetails(loginName: String, completion: @escaping (Result<UserDetails, NetworkError>) -> Void) {
+        if let cachedUser = cachedUserDetails.object(forKey: loginName as NSString) as String? {
+            let user: UserDetails = JSONUtil.jsonToObject(json: cachedUser)
+            completion(Result.success(user))
+            return
+        }
+        UserDetails.loadDummyResponse { [unowned self] (result) in
+            completion(result)
+            switch result {
+            case .success(let userDetails):
+                let userJSON = JSONUtil.objectToJSON(object: userDetails) as NSString
+                let key = loginName as NSString
+                self.cachedUserDetails.setObject(userJSON, forKey: key)
+            default: break
+            }
+        }
     }
 }
