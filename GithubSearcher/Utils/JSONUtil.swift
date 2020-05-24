5c00dec276c2bd46b9821410ffbd5e0a65e5b81a
Hugo Flores <hfp.ing2@gmail.com>
2020-05-24 18:52:45 -0400

    Added coder and decoder methods

--- GithubSearcher/Utils/JSONUtil.swift
+++ GithubSearcher/Utils/JSONUtil.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 struct JSONUtil {
-    static func load<T>(name: String) -> T where T: Codable {
+    static func load<T>(name: String) -> T where T: Decodable {
         guard let url = Bundle.main.url(forResource: name, withExtension: "json") else {
             fatalError("No JSON file named \(name)")
         }
@@ -18,6 +18,24 @@ struct JSONUtil {
             return try JSONDecoder().decode(T.self, from: data)
         } catch let error {
             fatalError("Error decoding the JSON file \(name): \(error)")
+        }
+    }
+    
+    static func jsonToObject<T>(json: String) -> T where T: Decodable {
+        do {
+            let data = Data(json.utf8)
+            return try JSONDecoder().decode(T.self, from: data)
+        } catch let error {
+            fatalError("Error decoding JSON \(json): \(error)")
+        }
+    }
+
+    static func objectToJSON<T>(object: T) -> String where T: Encodable {
+        do {
+            let jsonData = try JSONEncoder().encode(object)
+            return String(data: jsonData, encoding: .utf8)!
+        } catch {
+            fatalError("Could not convert structure to JSON")
         }
     }
 }
