8a49fa5cc92fb3d23ddbedbcb2fa9ea851e50f79
Hugo Flores <hfp.ing2@gmail.com>
2020-05-24 12:49:41 -0400

    View model implementation in view

--- GithubSearcher/AppDelegate.swift
+++ GithubSearcher/AppDelegate.swift
@@ -15,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = UINavigationController(rootViewController: UserSearchView())
+        let viewModel = UserSearchViewModel()
+        window?.rootViewController = UINavigationController(rootViewController: UserSearchView(viewModel: viewModel))
         window?.makeKeyAndVisible()
         return true
     }
--- GithubSearcher/Views/UserSearchView.swift
+++ GithubSearcher/Views/UserSearchView.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class UserSearchView: UIViewController {
     
+    private var viewModel: UserSearchViewModel!
+    
     private let searchBar: UISearchBar = {
         let bar = UISearchBar()
         bar.placeholder = "Search for Users"
@@ -20,6 +22,29 @@ class UserSearchView: UIViewController {
         let table = UITableView()
         return table
     }()
+    
+    private let spinner = UIActivityIndicatorView(style: .large)
+    
+    private let spinnerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 0, alpha: 0.3)
+        return view
+    }()
+
+    init(viewModel: UserSearchViewModel) {
+        super.init(nibName: nil, bundle: nil)
+        self.viewModel = viewModel
+        setUpDataBinding()
+    }
+    
+    func setUpDataBinding() {
+        viewModel.setScreenMessageHandler = displayOnTable
+        viewModel.setActivityIndicatorHandler = setActivityIndicatorState
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,10 +59,51 @@ class UserSearchView: UIViewController {
     }
     
     private func setUpSearchBar() {
+        searchBar.delegate = self
         searchBar.addAtTopOf(parent: view)
     }
     
     private func setUpTable() {
         tableView.addToAndFill(parent: view, belowOf: searchBar)
+        displayOnTable(message: viewModel.noQueryMessage)
+    }
+    
+    @objc private func executeNewSearch() {
+        viewModel.getNewData()
+    }
+    
+    private func displayOnTable(message: String?) {
+        guard let message = message else {
+            // tableView.separatorStyle = .singleLine
+            tableView.backgroundView = nil
+            return
+        }
+        let messageLabel = UILabel()
+        messageLabel.text = message
+        messageLabel.numberOfLines = 0;
+        messageLabel.textAlignment = .center;
+        messageLabel.font = UIFont.systemFont(ofSize: 30)
+        tableView.separatorStyle = .none
+        tableView.backgroundView = messageLabel
+    }
+    
+    private func setActivityIndicatorState(isShowing: Bool) {
+        if isShowing {
+            spinner.addToAndFill(parent: spinnerView)
+            spinnerView.addToParentAndFillChild(parent: view, child: tableView)
+            spinner.startAnimating()
+            return
+        }
+        spinner.stopAnimating()
+        spinner.removeFromSuperview()
+        spinnerView.removeFromSuperview()
+    }
+}
+
+extension UserSearchView: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        viewModel.setNewQuery(query: searchText)
+        NSObject.cancelPreviousPerformRequests(withTarget: self, selector: #selector(executeNewSearch), object: nil)
+        perform(#selector(executeNewSearch), with: nil, afterDelay: 0.4)
     }
 }
