ff0fe21f851fb5d5730b91ae101b7a3871bdf1b2
Hugo Flores <hfp.ing2@gmail.com>
2020-05-24 18:59:13 -0400

    View model implementation

--- GithubSearcher/Views/Cells/UserSearchTableCellView.swift
+++ GithubSearcher/Views/Cells/UserSearchTableCellView.swift
@@ -11,11 +11,13 @@ import UIKit
 class UserSearchTableCellView: UITableViewCell {
     
     static let reuseIdentifier = "UserSearchTableCellView"
-    
+
+    private var viewModel: UserCell?
+
     private let userImage: UIImageView = {
-        let image = UIImageView(image: UIImage(systemName: "plus"))
+        let image = UIImageView()
         image.contentMode = .scaleAspectFit
-        image.constraintTo(width: 60, height: 60)
+        image.constraintTo(width: 90, height: 90)
         return image
     }()
 
@@ -24,7 +26,6 @@ class UserSearchTableCellView: UITableViewCell {
         label.font = UIFont.boldSystemFont(ofSize: 20)
         label.textAlignment = .left
         label.numberOfLines = 2
-        label.text = "Hello"
         return label
     }()
     
@@ -33,7 +34,7 @@ class UserSearchTableCellView: UITableViewCell {
         label.font = UIFont.systemFont(ofSize: 14)
         label.textAlignment = .right
         label.numberOfLines = 1
-        label.text = "12 repos"
+        label.text = "..."
         label.setContentHuggingPriority(.required, for: .horizontal)
         return label
     }()
@@ -46,36 +47,62 @@ class UserSearchTableCellView: UITableViewCell {
         view.layoutMargins = UIEdgeInsets(top: 6, left: 12, bottom: 6, right: 12)
         return view
     }()
-    
+
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .medium)
+        spinner.setContentHuggingPriority(.required, for: .horizontal)
+        return spinner
+    }()
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        print("I am here2?")
-        setUpViews()
+        setUpMainContainer()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        print("I am here?")
-        setUpViews()
-    }
-    
-    func setUpViews() {
-//        userImage.translatesAutoresizingMaskIntoConstraints = false
-//        contentView.addSubview(userImage)
-//        userImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
-//        userImage.widthAnchor.constraint(equalToConstant: 60).isActive = true
-//        userImage.heightAnchor.constraint(equalToConstant: 60).isActive = true
-        setUpMainContainer()
-    }
-    
-    func setUpMainContainer() {
+    private func setUpMainContainer() {
         mainContainer.addToAndFill(parent: contentView)
         mainContainer.addArrangedSubview(userImage)
         mainContainer.addArrangedSubview(nameLabel)
+        mainContainer.addArrangedSubview(spinner)
+    }
+
+    func setUp(viewModel: UserCell) {
+        self.viewModel = viewModel
+        setUpDataBinding()
+        setInitialValues()
+    }
+    
+    private func setUpDataBinding() {
+        viewModel?.setImageHandler = setAvatarImage
+        viewModel?.setActivityIndicatorHandler = setActivityIndicatorState
+        viewModel?.setUserReposHandler = setUserRepos
+    }
+    
+    private func setInitialValues() {
+        nameLabel.text = viewModel?.getUserName()
+        //spinner.startAnimating()
+    }
+    
+    private func setAvatarImage(imageData: Data) {
+        userImage.image = UIImage(data: imageData)
+    }
+    
+    private func setActivityIndicatorState(isShowing: Bool) {
+        if isShowing {
+            spinner.startAnimating()
+        } else {
+            mainContainer.removeArrangedSubview(spinner)
+            spinner.stopAnimating()
+        }
+        
+    }
+    
+    private func setUserRepos(text: String) {
+        repoNumberLabel.text = text
         mainContainer.addArrangedSubview(repoNumberLabel)
     }
 }
