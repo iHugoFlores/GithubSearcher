246ab1ffc4663fc7f16320e1867d6635de4e0e31
Hugo Flores <hfp.ing2@gmail.com>
2020-05-24 18:52:05 -0400

    Custom keys and dummy data fetcher

--- GithubSearcher/Models/UserDetails.swift
+++ GithubSearcher/Models/UserDetails.swift
@@ -27,5 +27,38 @@ struct UserDetails: Codable {
     let hireable: Bool
     let bio: String
     let publicRepos, publicGists, followers, following: Int
-    let createdAt, updatedAt: Date
+    let createdAt, updatedAt: String
+
+    private enum CodingKeys: String, CodingKey {
+        case login, id, url, type, name, company
+        case blog, location, email, hireable, bio, followers, following
+        case nodeID = "node_id"
+        case avatarURL = "avatar_url"
+        case gravatarID = "gravatar_id"
+        case htmlURL = "html_url"
+        case followersURL = "followers_url"
+        case followingURL = "following_url"
+        case gistsURL = "gists_url"
+        case starredURL = "starred_url"
+        case subscriptionsURL = "subscriptions_url"
+        case organizationsURL = "organizations_url"
+        case reposURL = "repos_url"
+        case eventsURL = "events_url"
+        case receivedEventsURL = "received_events_url"
+        case siteAdmin = "site_admin"
+        case publicRepos = "public_repos"
+        case publicGists = "public_gists"
+        case createdAt = "created_at"
+        case updatedAt = "updated_at"
+    }
+}
+
+extension UserDetails {
+    typealias Completion = (Result<Self, NetworkError>) -> Void
+    static func loadDummyResponse(callback: @escaping Completion) {
+        DispatchQueue.global().asyncAfter(deadline: .now() + 0.2) {
+            let data: Self = JSONUtil.load(name: "UserDetails")
+            callback(Result.success(data))
+        }
+    }
 }
