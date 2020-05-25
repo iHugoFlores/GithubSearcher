35995d800584ab76e2f5bae33f232160c9f2bd1a
Hugo Flores <hfp.ing2@gmail.com>
2020-05-25 11:36:49 -0400

    Search and table handling

--- GithubSearcher/View Models/UserDetailsViewModel.swift
+++ GithubSearcher/View Models/UserDetailsViewModel.swift
@@ -10,9 +10,57 @@ import UIKit
 
 class UserDetailsViewModel {
     
+    let noQueryMessage = "Type on the search bar to search repos"
+    
     private var userDetails: UserDetails!
     
     private var avatarImage: Data!
+    
+    private var repoQuery: String? {
+        didSet {
+            guard let value = repoQuery else {
+                isDataDownloading = false
+                displayMessage = noQueryMessage
+                return
+            }
+            if value.isEmpty {
+                isDataDownloading = false
+                displayMessage = noQueryMessage
+                repoQuery = nil
+                repos = []
+                return
+            }
+            isDataDownloading = true
+            displayMessage = nil
+        }
+    }
+    
+    private var displayMessage: String? {
+        didSet {
+            guard let handler = setScreenMessageHandler else { return }
+            handler(displayMessage)
+        }
+    }
+    
+    private var isDataDownloading: Bool = false {
+        didSet {
+            if oldValue == isDataDownloading { return }
+            guard let handler = setActivityIndicatorHandler else { return }
+            handler(isDataDownloading)
+        }
+    }
+    
+    private var repos: [RepositoryCell] = [] {
+        didSet {
+            isDataDownloading = false
+            guard let handler = reloadTableHandler else { return }
+            handler()
+        }
+    }
+    
+    var setActivityIndicatorHandler: ((Bool) -> Void)?
+    var setScreenMessageHandler: ((String?) -> Void)?
+    var reloadTableHandler: (() -> Void)?
     
     init(userDetails: UserDetails, avatar: Data) {
         self.userDetails = userDetails
@@ -44,7 +92,7 @@ class UserDetailsViewModel {
         }
         
         stringContent.append(NSAttributedString(
-            string: "\(userDetails.email)\n\(userDetails.location)\n\(joinDate)\n",
+            string: "\(userDetails.email ?? "No Email")\n\(userDetails.location ?? "No Location")\n\(joinDate)\n",
             attributes: [
                 NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16),
                 NSAttributedString.Key.foregroundColor: UIColor.gray]
@@ -60,6 +108,36 @@ class UserDetailsViewModel {
     }
     
     func getUserBiography() -> String {
-        return userDetails.bio
+        return userDetails.bio ?? "No biography"
+    }
+    
+    func setNewQuery(query: String) {
+        repoQuery = query
+    }
+    
+    func getNewData() {
+        if repoQuery == nil { return }
+        UserRepository.loadDummyResponse { [unowned self] (result) in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let data):
+                    self.repos = self.parseResponseToRepositoryCell(userRepositories: data)
+                case .failure(let error):
+                    print(error)
+                }
+            }
+        }
+    }
+    
+    private func parseResponseToRepositoryCell(userRepositories: [UserRepository]) -> [RepositoryCell] {
+        return userRepositories.map { RepositoryCell(repository: $0) }
+    }
+    
+    func getRepoViewModelAt(indexPath: IndexPath) -> RepositoryCell {
+        return repos[indexPath.row]
+    }
+    
+    func getNumberOfRepos() -> Int {
+        return repos.count
     }
 }
--- GithubSearcher/Views/UserDetailsView.swift
+++ GithubSearcher/Views/UserDetailsView.swift
@@ -28,33 +28,64 @@ class UserDetailsView: UIViewController {
     
     private let biograpgyLabel: UILabel = {
         let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
         label.font = UIFont.systemFont(ofSize: 20)
         label.textAlignment = .center
         label.numberOfLines = 0
+        label.setContentHuggingPriority(.required, for: .vertical)
         return label
     }()
     
     private let topContainer: UIStackView = {
         let view = UIStackView()
-        view.translatesAutoresizingMaskIntoConstraints = false
         view.axis = .horizontal
         view.spacing = 6
         view.distribution = .fillEqually
         view.alignment = .center
         view.isLayoutMarginsRelativeArrangement = true
         view.layoutMargins = UIEdgeInsets(top: 6, left: 12, bottom: 6, right: 12)
+        view.setContentHuggingPriority(.required, for: .vertical)
+        return view
+    }()
+
+    private let searchBar: UISearchBar = {
+        let bar = UISearchBar()
+        bar.placeholder = "Search for User's Repositories"
+        bar.setContentHuggingPriority(.required, for: .vertical)
+        return bar
+    }()
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.tableFooterView = UIView()
+        table.estimatedRowHeight = 10
+        table.rowHeight = UITableView.automaticDimension
+        table.setContentCompressionResistancePriority(.required, for: .vertical)
+        return table
+    }()
+    
+    private let spinner = UIActivityIndicatorView(style: .large)
+    
+    private let spinnerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 0, alpha: 0.3)
         return view
     }()
 
     init(viewModel: UserDetailsViewModel) {
         super.init(nibName: nil, bundle: nil)
         self.viewModel = viewModel
-        
+        setUpDataBinding()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUpDataBinding() {
+        viewModel.setActivityIndicatorHandler = setActivityIndicatorState
+        viewModel.setScreenMessageHandler = displayOnTable
+        viewModel.reloadTableHandler = reloadTableData
     }
     
     override func viewDidLoad() {
@@ -67,23 +98,99 @@ class UserDetailsView: UIViewController {
     }
     
     private func setUpViews() {
-        view.addSubview(topContainer)
-        topContainer.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        topContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        topContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-
+        topContainer.addAtTopOf(parent: view)
         topContainer.addArrangedSubview(userImage)
         topContainer.addArrangedSubview(userDataLabel)
 
-        view.addSubview(biograpgyLabel)
-        biograpgyLabel.topAnchor.constraint(equalTo: topContainer.bottomAnchor).isActive = true
-        biograpgyLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        biograpgyLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true        
+        biograpgyLabel.addAtTopOf(parent: view, below: topContainer)
+        
+        setUpSearchBar()
+        setUpTable()
+    }
+    
+    private func setUpSearchBar() {
+        searchBar.delegate = self
+        searchBar.addAtTopOf(parent: view, below: biograpgyLabel)
+    }
+    
+    private func setUpTable() {
+        tableView.dataSource = self
+        tableView.register(RepositoryTableCellView.self, forCellReuseIdentifier: RepositoryTableCellView.reuseIdentifier)
+        tableView.addToAndFill(parent: view, belowOf: searchBar)
     }
     
     private func setUpInitData() {
         userImage.image = UIImage(data: viewModel.getUserAvatar())
         userDataLabel.attributedText = viewModel.getUserDescription()
         biograpgyLabel.text = viewModel.getUserBiography()
+        displayOnTable(message: viewModel.noQueryMessage)
+    }
+    
+    private func displayOnTable(message: String?) {
+        guard let message = message else {
+            tableView.backgroundView = nil
+            return
+        }
+        let messageLabel = UILabel()
+        messageLabel.text = message
+        messageLabel.numberOfLines = 0;
+        messageLabel.textAlignment = .center;
+        messageLabel.font = UIFont.systemFont(ofSize: 30)
+        tableView.backgroundView = messageLabel
+    }
+    
+    private func setActivityIndicatorState(isShowing: Bool) {
+        if isShowing {
+            spinner.addToAndCenter(parent: spinnerView)
+            spinner.startAnimating()
+            UIView.transition(
+                with: view,
+                duration: 0.33,
+                options: .transitionCrossDissolve,
+                animations: {
+                    self.spinnerView.addToParentAndFillChild(parent: self.view, child: self.tableView)
+            },
+                completion: nil)
+            return
+        }
+        spinner.stopAnimating()
+        spinner.removeFromSuperview()
+        UIView.transition(
+            with: view,
+            duration: 0.33,
+            options: .transitionCrossDissolve,
+            animations: {
+                self.spinnerView.removeFromSuperview()
+        },
+            completion: nil)
+    }
+    
+    private func reloadTableData() {
+        tableView.reloadData()
+    }
+    
+    @objc private func executeNewSearch() {
+        viewModel.getNewData()
+    }
+}
+
+extension UserDetailsView: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        viewModel.setNewQuery(query: searchText)
+        NSObject.cancelPreviousPerformRequests(withTarget: self, selector: #selector(executeNewSearch), object: nil)
+        perform(#selector(executeNewSearch), with: nil, afterDelay: 0.4)
+    }
+}
+
+extension UserDetailsView: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.getNumberOfRepos()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: RepositoryTableCellView.reuseIdentifier, for: indexPath) as! RepositoryTableCellView
+        let repoViewModel = viewModel.getRepoViewModelAt(indexPath: indexPath)
+        cell.setUp(viewModel: repoViewModel)
+        return cell
     }
 }
