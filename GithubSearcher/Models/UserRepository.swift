6911163dd98e1d4310d5bd323473e35cf3f1fa5e
Hugo Flores <hfp.ing2@gmail.com>
2020-05-25 17:29:39 -0400

    API Call implemented

--- GithubSearcher/Models/UserRepository.swift
+++ GithubSearcher/Models/UserRepository.swift
@@ -24,10 +24,38 @@ struct UserRepository: Codable {
 
 extension UserRepository {
     typealias Completion = (Result<[Self], NetworkError>) -> Void
+    typealias CompletionWithResponse = (Result<[Self], NetworkError>, HTTPURLResponse?) -> Void
+    private static let pageSize = 100
+    private static var endpoint: URL = {
+        var urlC = URLComponents()
+        urlC.scheme = "https"
+        urlC.host = "api.github.com"
+        urlC.path = "/users"
+        return urlC.url!
+    }()
+
     static func loadDummyResponse(callback: @escaping Completion) {
         DispatchQueue.global().asyncAfter(deadline: .now() + 0.2) {
             let data: [Self] = JSONUtil.load(name: "UserRepositories")
             callback(Result.success(data))
         }
+    }
+    
+    static func getUserRepos(networkManager: NetworkManager, user: String, page: Int, callback: @escaping CompletionWithResponse) {
+        guard var urlC = URLComponents(url: endpoint, resolvingAgainstBaseURL: true) else { return }
+        urlC.path += "/\(user)/repos"
+        urlC.queryItems = Self.getQueryParameters(page: page)
+        guard let url = urlC.url else { return }
+        print(url)
+        networkManager.getRESTDataFrom(url: url) { (result: Result<[Self], NetworkError>, response) in
+            callback(result, response)
+        }
+    }
+    
+    private static func getQueryParameters(page: Int) -> [URLQueryItem] {
+        return [
+            URLQueryItem(name: "page", value: String(page)),
+            URLQueryItem(name: "per_page", value: String(pageSize))
+        ]
     }
 }
