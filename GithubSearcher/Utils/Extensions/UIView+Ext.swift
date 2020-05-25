cb97c621252b9fca001c3ed7b399474b355ef7c5
Hugo Flores <hfp.ing2@gmail.com>
2020-05-25 11:35:56 -0400

    Refactor and center method

--- GithubSearcher/Utils/Extensions/UIView+Ext.swift
+++ GithubSearcher/Utils/Extensions/UIView+Ext.swift
@@ -9,17 +9,27 @@
 import UIKit
 
 extension UIView {
-    func addAtTopOf(parent: UIView) {
+    private func addToParentAndActivateAutolayout(parent: UIView) {
         self.translatesAutoresizingMaskIntoConstraints = false
         parent.addSubview(self)
+    }
+
+    func addAtTopOf(parent: UIView) {
+        addToParentAndActivateAutolayout(parent: parent)
         self.trailingAnchor.constraint(equalTo: parent.trailingAnchor).isActive = true
         self.leadingAnchor.constraint(equalTo: parent.leadingAnchor).isActive = true
         self.topAnchor.constraint(equalTo: parent.safeAreaLayoutGuide.topAnchor).isActive = true
     }
+    
+    func addAtTopOf(parent: UIView, below: UIView) {
+        addToParentAndActivateAutolayout(parent: parent)
+        self.trailingAnchor.constraint(equalTo: parent.trailingAnchor).isActive = true
+        self.leadingAnchor.constraint(equalTo: parent.leadingAnchor).isActive = true
+        self.topAnchor.constraint(equalTo: below.bottomAnchor).isActive = true
+    }
 
     func addToAndFill(parent: UIView, belowOf reference: UIView) {
-        self.translatesAutoresizingMaskIntoConstraints = false
-        parent.addSubview(self)
+        addToParentAndActivateAutolayout(parent: parent)
         self.trailingAnchor.constraint(equalTo: parent.trailingAnchor).isActive = true
         self.leadingAnchor.constraint(equalTo: parent.leadingAnchor).isActive = true
         self.topAnchor.constraint(equalTo: reference.bottomAnchor).isActive = true
@@ -27,8 +37,7 @@ extension UIView {
     }
 
     func addToAndFill(parent: UIView) {
-        self.translatesAutoresizingMaskIntoConstraints = false
-        parent.addSubview(self)
+        addToParentAndActivateAutolayout(parent: parent)
         self.trailingAnchor.constraint(equalTo: parent.trailingAnchor).isActive = true
         self.leadingAnchor.constraint(equalTo: parent.leadingAnchor).isActive = true
         self.topAnchor.constraint(equalTo: parent.topAnchor).isActive = true
@@ -36,8 +45,7 @@ extension UIView {
     }
 
     func addToParentAndFillChild(parent: UIView, child: UIView) {
-        self.translatesAutoresizingMaskIntoConstraints = false
-        parent.addSubview(self)
+        addToParentAndActivateAutolayout(parent: parent)
         self.trailingAnchor.constraint(equalTo: child.trailingAnchor).isActive = true
         self.leadingAnchor.constraint(equalTo: child.leadingAnchor).isActive = true
         self.topAnchor.constraint(equalTo: child.topAnchor).isActive = true
@@ -52,5 +60,11 @@ extension UIView {
         let heightConstraint = self.heightAnchor.constraint(equalToConstant: height)
         heightConstraint.priority = UILayoutPriority(999)
         heightConstraint.isActive = true
+    }
+    
+    func addToAndCenter(parent: UIView) {
+        addToParentAndActivateAutolayout(parent: parent)
+        self.centerYAnchor.constraint(equalTo: parent.centerYAnchor).isActive = true
+        self.centerXAnchor.constraint(equalTo: parent.centerXAnchor).isActive = true
     }
 }
