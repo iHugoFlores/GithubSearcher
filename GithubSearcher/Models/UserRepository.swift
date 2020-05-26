d84d7086fd28e7254f2e7f25f40567d483827d4a
Hugo Flores <hfp.ing2@gmail.com>
2020-05-26 18:46:58 -0400

    Reading credentials from keychain

--- GithubSearcher/Models/UserRepository.swift
+++ GithubSearcher/Models/UserRepository.swift
@@ -39,16 +39,15 @@ extension UserRepository {
         urlC.path += "/\(user)/repos"
         urlC.queryItems = Self.getQueryParameters(page: page)
         guard let url = urlC.url else { return }
-        if let storedUser = UserDefaults.standard.string(forKey: Constants.UserDefaults.USER),
-            let storedPassword = UserDefaults.standard.string(forKey: Constants.UserDefaults.PASSWORD) {
-            let credentials = "\(storedUser):\(storedPassword)"
+        do {
+            let credentials = try KeychainManager.getPassword(service: Constants.Keychain.service, account: Constants.Keychain.account)
             networkManager.getRESTDataFrom(url: url, credentials: credentials) { (result: Result<[Self], NetworkError>, response) in
                 callback(result, response)
             }
-            return
-        }
-        networkManager.getRESTDataFrom(url: url) { (result: Result<[Self], NetworkError>, response) in
-            callback(result, response)
+        } catch {
+            networkManager.getRESTDataFrom(url: url) { (result: Result<[Self], NetworkError>, response) in
+                callback(result, response)
+            }
         }
     }
     
