8a5655099d815c5f342398a5c7e9269028e84c83
Hugo Flores <hfp.ing2@gmail.com>
2020-05-26 11:22:18 -0400

    App constants

---
+++ GithubSearcher/Utils/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  GithubSearcher
+//
+//  Created by Hugo Flores Perez on 5/26/20.
+//  Copyright © 2020 Hugo Flores Perez. All rights reserved.
+//
+
+import Foundation
+
+final class Constants {
+    final class UserDefaults {
+        static let USER = "USER"
+        static let PASSWORD = "PASSWORD"
+    }
+}
