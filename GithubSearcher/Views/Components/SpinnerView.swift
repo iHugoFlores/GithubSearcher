9bc7169eb1e5174a7819b74ff0abdbdb68b7b89a
Hugo Flores <hfp.ing2@gmail.com>
2020-05-26 11:23:36 -0400

    Custom components

---
+++ GithubSearcher/Views/Components/SpinnerView.swift
@@ -0,0 +1,51 @@
+//
+//  SpinnerView.swift
+//  GithubSearcher
+//
+//  Created by Hugo Flores Perez on 5/26/20.
+//  Copyright © 2020 Hugo Flores Perez. All rights reserved.
+//
+
+import UIKit
+
+class SpinnerView: UIView {
+
+    private let spinner = UIActivityIndicatorView(style: .large)
+    
+    init() {
+        super.init(frame: .zero)
+        backgroundColor = UIColor(white: 0, alpha: 0.3)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func presentOn(parent: UIView) {
+        spinner.addToAndCenter(parent: self)
+        spinner.startAnimating()
+        UIView.transition(
+            with: parent,
+            duration: 0.33,
+            options: .transitionCrossDissolve,
+            animations: {
+                self.addToAndFill(parent: parent)
+        },
+            completion: nil)
+    }
+    
+    func stop() {
+        guard let superview = superview else { print("No superview"); return }
+        spinner.stopAnimating()
+        spinner.removeFromSuperview()
+        UIView.transition(
+            with: superview,
+            duration: 0.33,
+            options: .transitionCrossDissolve,
+            animations: {
+                self.removeFromSuperview()
+        },
+            completion: nil)
+        spinner.stopAnimating()
+    }
+}
