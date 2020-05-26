4876dc5de20844f0229ab766d0f9291e6ffe9bd8
Hugo Flores <hfp.ing2@gmail.com>
2020-05-26 11:22:57 -0400

    Added request with authentication

--- GithubSearcher/Utils/Managers/NetworkManager.swift
+++ GithubSearcher/Utils/Managers/NetworkManager.swift
@@ -9,12 +9,13 @@
 import UIKit
 
 class NetworkManager {
-    private var networkHandler: NetworkProtocol
-    
+    typealias DataCompletion = (Result<Data, NetworkError>, HTTPURLResponse?) -> Void
+    var networkHandler: NetworkProtocol
+
     init(networkHandler: NetworkProtocol) {
         self.networkHandler = networkHandler
     }
-    
+
     func getRESTDataFrom<T>(url: URL, completion: @escaping (Result<T, NetworkError>, HTTPURLResponse?) -> Void) where T: Decodable {
         getDataFrom(url: url.absoluteString) { (result, response) in
             switch result {
@@ -30,44 +31,52 @@ class NetworkManager {
             }
         }
     }
-    
-    func getDataFrom(url: String, completion: @escaping (Result<Data, NetworkError>, HTTPURLResponse?) -> Void) {
+
+    func getDataFrom(url: String, completion: @escaping DataCompletion) {
         guard let parsedURL = URL(string: url) else {
             fatalError("Invalid URL string: \(url)")
         }
-        networkHandler.getData(url: parsedURL) { (data, response, error) in
-            if let error = error as NSError?, error.domain == NSURLErrorDomain {
-                switch error.code {
-                case NSURLErrorNotConnectedToInternet:
-                    completion(Result.failure(.deviceOffline), nil)
-                default:
-                    completion(Result.failure(.unexpectedError(error)), nil)
-                }
-                return
-            }
-            guard let response = response as? HTTPURLResponse else {
-                completion(Result.failure(.noResponseError), nil)
-                return
-            }
-            if response.statusCode != 200 {
-                completion(Result.failure(self.getSpecificServerError(from: response)), response)
-                return
-            }
-            guard let data = data else {
-                completion(Result.failure(.emptyResult), response)
-                return
-            }
-            completion(Result.success(data), response)
-        }
+        networkHandler.getData(url: parsedURL) { self.handleResponse(data: $0, response: $1, error: $2, completion: completion) }
     }
-    
+
+    func getDataFrom(url: String, credentials: String?, completion: @escaping DataCompletion) {
+        guard let parsedURL = URL(string: url) else {
+            fatalError("Invalid URL string: \(url)")
+        }
+        networkHandler.getData(url: parsedURL, withAuth: credentials) { self.handleResponse(data: $0, response: $1, error: $2, completion: completion) }
+    }
+
+    private func handleResponse(data: Data?, response: URLResponse?, error: Error?, completion: @escaping DataCompletion) {
+        if let error = error as NSError?, error.domain == NSURLErrorDomain {
+            switch error.code {
+            case NSURLErrorNotConnectedToInternet:
+                completion(Result.failure(.deviceOffline), nil)
+            default:
+                completion(Result.failure(.unexpectedError(error)), nil)
+            }
+            return
+        }
+        guard let response = response as? HTTPURLResponse else {
+            completion(Result.failure(.noResponseError), nil)
+            return
+        }
+        if response.statusCode != 200 {
+            completion(Result.failure(self.getSpecificServerError(from: response)), response)
+            return
+        }
+        guard let data = data else {
+            completion(Result.failure(.emptyResult), response)
+            return
+        }
+        completion(Result.success(data), response)
+    }
+
     private func getSpecificServerError(from response: HTTPURLResponse) -> NetworkError {
         let errorCode = response.statusCode
         switch errorCode {
-        case 403:
-            return .rateLimitReached
-        default:
-            return .serverError
+        case 401: return .unauthorized
+        case 403: return .rateLimitReached
+        default: return .serverError
         }
     }
 }
